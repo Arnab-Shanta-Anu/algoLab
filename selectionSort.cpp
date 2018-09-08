a529a013015b53072527dbe0f178cbd670a64c9b
Arnab-Shanta-Anu <arnabshantaanu@gmail.com>
2018-09-09 01:04:07 +0600

    added complexity check for 10,50,.. data size

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -1,82 +1,143 @@
 #include <bits/stdc++.h>
+#include <iomanip>
+#include <chrono>
 #include <vector>
 #include "randomInput.h"
 #include "printVec.h"
 
 using namespace std;
-//template<typename T, typename A>
-vector<int> bubbleSort(vector<int>);
-//template<typename T, typename A>
-vector<int> insertionSort(vector<int>);
-//template<typename T, typename A>
-vector<int> selectionSort(vector<int>);
+template<typename T, typename A>
+vector<T> bubbleSort(vector<T,A>);
+template<typename T, typename A>
+vector<T> insertionSort(vector<T,A>);
+template<typename T, typename A>
+vector<T> selectionSort(vector<T,A>);
+
+int dataSize[8] = {10,50,100,500,1000,2000,5000,10000};
 
 int main()
 {
-    vector<int> inputs,sorted;
-    inputs = generateRandomNumber(10);
+    freopen("out.txt","w",stdout);
 
-    print(inputs);
+    for(int x=0;x<8;x++){
+    cout<<"Data Size: "<<dataSize[x]<<endl;
+
+    //cout<<fixed<<setprecision(6);
+
+    vector<int> avg,best,worst,sorted;
+    avg    = generateRandomNumber<int>(dataSize[x]);
+    best   = generateAscendingVector<int>(dataSize[x]);
+    worst = generateDecendingVector<int>(dataSize[x]);
 
 
-    //bubblesort
+    /**bubblesort**/
+
+//best case
+    sorted.clear();
+    clock_t start = clock();
+    sorted = bubbleSort<int>(best);
+    clock_t en = clock();
+    double duration =  double(en-start)/CLOCKS_PER_SEC;
+    cout<<"Bubble Sort duration(best)    : "<<duration<<endl;
+
+//average case
 
     sorted.clear();
-    time_t start = clock();
-    sorted = bubbleSort(inputs);
-    time_t en = clock();
-
-    print(sorted);
-    sorted.clear();
-    //print(sorted);
-    double duration = double(en-start)/CLOCKS_PER_SEC;
-    cout<<"duration: "<<duration<<endl;
-
-
-    //insertionsort
-
-
     start = clock();
-    sorted = insertionSort(inputs);
+    sorted = bubbleSort<int>(avg);
     en = clock();
-
-    print(sorted);
-    sorted.clear();
-
     duration = double(en-start)/CLOCKS_PER_SEC;
-    cout<<"duration: "<<duration<<endl;
+    cout<<"Bubble Sort duration(avg)     : "<<duration<<endl;
 
+//worst case
 
-    //selectionsort
-
-
+    sorted.clear();
     start = clock();
-    sorted = selectionSort(inputs);
+    sorted = bubbleSort<int>(worst);
     en = clock();
-
-    print(sorted);
-    sorted.clear();
     duration = double(en-start)/CLOCKS_PER_SEC;
-    cout<<"duration: "<<duration<<endl;
+    cout<<"Bubble Sort duration(worst)   : "<<duration<<endl;
+    cout<<endl;
 
+    /**insertionsort**/
+
+//best case
+
+    sorted.clear();
+    start = clock();
+    sorted = insertionSort<int>(best);
+    en = clock();
+    duration = double(en-start)/CLOCKS_PER_SEC;
+    cout<<"Insertion Sort duration(best) : "<<duration<<endl;
+
+//average case
+
+    sorted.clear();
+    start = clock();
+    sorted = insertionSort<int>(avg);
+    en = clock();
+    duration = double(en-start)/CLOCKS_PER_SEC;
+    cout<<"Insertion Sort duration(avg)  : "<<duration<<endl;
+
+//worst case
+
+    sorted.clear();
+    start = clock();
+    sorted = insertionSort<int>(worst);
+    en = clock();
+    duration = double(en-start)/CLOCKS_PER_SEC;
+    cout<<"Insertion Sort duration(worst): "<<duration<<endl;
+    cout<<endl;
+
+    /**selectionsort**/
+
+//best case
+    sorted.clear();
+    start = clock();
+    sorted = selectionSort<int>(best);
+    en = clock();
+    duration = double(en-start)/CLOCKS_PER_SEC;
+    cout<<"Selection Sort duration(best) : "<<duration<<endl;
+
+//average case
+
+    sorted.clear();
+    start = clock();
+    sorted = selectionSort<int>(avg);
+    en = clock();
+    duration = double(en-start)/CLOCKS_PER_SEC;
+    cout<<"Selection Sort duration(avg)  : "<<duration<<endl;
+
+//worst case
+
+    sorted.clear();
+    start = clock();
+    sorted = selectionSort<int>(worst);
+    en = clock();
+    duration = double(en-start)/CLOCKS_PER_SEC;
+    cout<<"Selection Sort duration(worst): "<<duration<<endl;
+    cout<<endl;
+
+    }
     return 0;
 }
 
-//template<typename T, typename A>
-vector<int> bubbleSort(vector<int> arr)
+template<typename T, typename A>
+vector<T> bubbleSort(vector<T,A> arr)
 {
     int n=arr.size();
     for(int i=0;i<n;i++){
-        for(int j=0;j<n-i;j++){
-            if(arr[j]>arr[j+1])
-            swap(arr[j],arr[j+1]);
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
         }
     }
     return arr;
 }
 
-//template<typename T, typename A>
-vector<int> insertionSort(vector<int> arr)
+template<typename T, typename A>
+vector<T> insertionSort(vector<T,A> arr)
 {
     int tmp,j,n=arr.size();
     for(int i=0;i<n;i++){
@@ -91,15 +152,17 @@ vector<int> insertionSort(vector<int> arr)
     return arr;
 }
 
-//template<typename T, typename A>
-vector<int> selectionSort(vector<int> arr)
+template<typename T, typename A>
+vector<T> selectionSort(vector<T,A> arr)
 {
     int n=arr.size();
     for(int i=0;i<n;i++){
+        int highIndex = 0;
         for(int j=0;j<n-i;j++){
-            if(arr[j]>arr[j+1])
-            swap(arr[j],arr[j+1]);
+            if(arr[j]>arr[highIndex])
+                highIndex = j;
         }
+        swap(arr[highIndex],arr[n-i-1]);
     }
     return arr;
 }
