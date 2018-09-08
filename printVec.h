09329448fd2228c2b8a07684e287049e7af8f11e
Arnab-Shanta-Anu <arnabshantaanu@gmail.com>
2018-09-09 00:56:22 +0600

    gave it a template argument

--- printVec.h
+++ printVec.h
@@ -2,12 +2,12 @@
 #define PRINTVEC_H
 
 using namespace std;
-//template <typename T, typename A>
-void print(vector<int> const &arr)
+template <typename T, typename A>
+void print(vector<T,A> const &arr)
 {
     int n = arr.size();
     for(int i=0;i<n;i++)
-        cout<<arr.at(i)<<endl;
+        cout<<arr.at(i)<<"\t";
     cout<<endl;
 }
 
